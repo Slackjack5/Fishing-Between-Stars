67beee19cd2e6e1e5d957214afdf3631675c775b
Nycelly <dkr264@nyu.edu>
2020-10-17 03:35:52 -0400

    Made my own scene for testing fishing scripts.
    Udon sharp be purty good.

---
+++ Fishing Between Stars Project/Assets/Nycelly Fishing/FishingCube.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FishingCube : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, 90f * Time.deltaTime);
+    }
+}
