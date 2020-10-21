9e9f2616cc72bbc9624d881ce870025044db9f19
Nycelly <dkr264@nyu.edu>
2020-10-20 23:56:29 -0400

    Made a simple script that detects collision!
    Cube Detects Collision of a Water block and turns red.

--- Fishing Between Stars Project/Assets/Nycelly Fishing/FishingCube.cs
+++ Fishing Between Stars Project/Assets/Nycelly Fishing/FishingCube.cs
@@ -6,13 +6,44 @@ using VRC.Udon;
 
 public class FishingCube : UdonSharpBehaviour
 {
+    public bool inWater = false;
+    public Renderer myCubeRenderer;
+    
     void Start()
     {
-        
+       
     }
 
     private void Update()
     {
         transform.Rotate(Vector3.up, 90f * Time.deltaTime);
+
+        if (inWater==true)
+        {
+            myCubeRenderer.material.SetColor("_Color", Color.red);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //Change the Name to what the Water Is
+        if(other.gameObject.name == "Test Water")
+        {
+            inWater = true;
+            Debug.Log("In the Water");
+        }
+
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        //Change the Name to what the Water Is
+        if (other.gameObject.name == "Test Water")
+        {
+            inWater = false;
+            Debug.Log("Out of Water");
+        }
+
     }
 }
